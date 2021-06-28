0700ffe7cf90b14b32f17597cf0e130c5a29508c
jakeogh <github.com@v6y.net>
2021-06-28 00:01:52 -0700

    auto-commit

--- unicodehaz/unicodehaz.py
+++ unicodehaz/unicodehaz.py
@@ -6,18 +6,23 @@ import click
 
 
 @click.command()
-@click.argument("codepoints", type=int, nargs=-1)
+@click.argument("codepoints", type=int, nargs=-1,)
 @click.option('--all', "all_codepoints",
               is_flag=True,
-              help="Include unnamed codepoints in output")
+              help="Include unnamed codepoints in output",)
 @click.option('--null', is_flag=True, help="NULL terminalted output")
 @click.option('--glyphs-only', is_flag=True, help="Do not print index numbers")
 @click.option('--stats', is_flag=True, help="Only print statistics")
-def cli(codepoints,
-        all_codepoints,
-        null,
-        glyphs_only,
-        stats,):
+@click.option('--start', type=int)
+@click.option('--stop', type=int)
+def cli(codepoints: tuple[int, ...],
+        all_codepoints: bool,
+        null: bool,
+        glyphs_only: bool,
+        stats: bool,
+        start: int,
+        stop: int,
+        ):
 
     # unicode is base 0x110000 1114112 https://wtanaka.com/node/8213
     named = not all_codepoints
@@ -31,6 +36,12 @@ def cli(codepoints,
     if codepoints:
         iterator = codepoints
     for index, point in enumerate(iterator):
+        if start:
+            if point < start:
+                continue
+        if stop:
+            if point > stop:
+                continue
         line = []
         thing = chr(point)
         try:
@@ -63,7 +74,3 @@ def cli(codepoints,
         print("Unnamed codepoints:  ", len(unnamed_codepoints))
         print()
         print(unicodedata.__doc__)
-
-
-if __name__ == "__main__":
-    cli()
